fe2206e1e68262462faaf5b91258a849495d0753
Attilius <leo20@freemail.hu>
2022-11-02 11:52:37 +0100

    Modified HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('home/home.html.twig', []);
