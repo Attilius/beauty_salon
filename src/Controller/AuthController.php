62aaca1c408088eef90a34025c274debbdb19ee7
Attilius <leo20@freemail.hu>
2022-11-03 19:29:04 +0100

    Modified AuthController.php

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -14,9 +14,9 @@ class AuthController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        //if ($this->getUser()) {
-        //    return $this->redirectToRoute('/');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
