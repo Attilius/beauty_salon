9a35ea716f9f5dfe9b5391f65af4b60c772defb1
Attilius <leo20@freemail.hu>
2022-11-08 19:42:51 +0100

    Modified AdminController.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +11,7 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="app_admin")
+     * @IsGranted("ROLE_ADMIN")
      * @return Response
      */
     public function show(): Response
