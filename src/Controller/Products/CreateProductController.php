f2a2b47374bebf1b65076005779407a6098d6ed3
Attilius <leo20@freemail.hu>
2022-11-09 22:07:27 +0100

    Added CreateProductController.php

---
+++ src/Controller/Products/CreateProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Products;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateProductController extends AbstractController
+{
+    /**
+     * @Route("/admin/product_create", name="app_create_product")
+     * @IsGranted("ROLE_ADMIN")
+     * @return Response
+     */
+    public function create(): Response
+    {
+        return $this->render('/products/create_product.html.twig', []);
+    }
+}
