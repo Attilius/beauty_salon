0fda89ef8a2143f7db7836f551be3c386f428653
Attilius <leo20@freemail.hu>
2022-11-10 21:36:09 +0100

    Modified dashboard-menu.js

--- public/js/dashboard-menu.js
+++ public/js/dashboard-menu.js
@@ -3,12 +3,19 @@
 const sidebarMenu = document.getElementById('sidebarMenu');
 const sidebarMenuItem = sidebarMenu.children[0].children[0].children;
 
+window.onload = function() {
+    const index = localStorage.getItem("menu_index");
+    console.log(index)
+}
 for (let i = 0; i < sidebarMenuItem.length; i++) {
     const item = sidebarMenuItem[i];
     const setActive = item => {
-        [...item.target.parentElement.parentElement.children]
+            [...item.target.parentElement.parentElement.children]
             .forEach(sib => sib.children[0].classList.remove('active'));
-        if (item.target.nodeName === 'A') item.target.classList.add('active');
+        if (item.target.nodeName === 'A') {
+            localStorage.setItem('menu_index', i);
+            item.target.classList.add('active');
+        }
         else {
             [...item.target.parentElement.parentElement.parentElement.children]
                 .forEach(sib => sib.children[0].classList.remove('active'));
