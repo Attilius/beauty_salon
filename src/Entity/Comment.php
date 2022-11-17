f97fc0b8181c5778b7fd0aef7e25da5df502b905
Attilius <leo20@freemail.hu>
2022-11-17 21:05:35 +0100

    Added Comment.php

---
+++ src/Entity/Comment.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $publishDate = null;
+
+    #[ORM\Column]
+    private ?int $upVote = null;
+
+    #[ORM\Column]
+    private ?int $downVote = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublishDate(): ?\DateTimeInterface
+    {
+        return $this->publishDate;
+    }
+
+    public function setPublishDate(\DateTimeInterface $publishDate): self
+    {
+        $this->publishDate = $publishDate;
+
+        return $this;
+    }
+
+    public function getUpVote(): ?int
+    {
+        return $this->upVote;
+    }
+
+    public function setUpVote(int $upVote): self
+    {
+        $this->upVote = $upVote;
+
+        return $this;
+    }
+
+    public function getDownVote(): ?int
+    {
+        return $this->downVote;
+    }
+
+    public function setDownVote(int $downVote): self
+    {
+        $this->downVote = $downVote;
+
+        return $this;
+    }
+}
