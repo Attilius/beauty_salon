f3f9305bf22f2666bc164979951d89900cf5836f
Attilius <leo20@freemail.hu>
2022-11-20 19:26:20 +0100

    Modified ProductTest.php

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -36,4 +36,14 @@ class ProductTest extends TestCase
         $product->setDescription("Lorem ipsum dolor sit amet...");
         $this->assertSame("Lorem ipsum dolor sit amet...", $product->getDescription());
     }
+
+    public function test_setting_image()
+    {
+        $product = new Product();
+
+        $this->assertNull($product->getImage());
+
+        $product->setImage("https://beauty-tools/tool_01.png");
+        $this->assertSame("https://beauty-tools/tool_01.png", $product->getImage());
+    }
 }
