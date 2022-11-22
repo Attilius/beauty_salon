97b0ad9e7e727f5f866ee1d100d5e1ce0444025e
Attilius <leo20@freemail.hu>
2022-11-22 20:23:26 +0100

    Added EmployeeFactory.php

---
+++ src/Factory/EmployeeFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Employee;
+use App\Repository\EmployeeRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Employee>
+ *
+ * @method static Employee|Proxy createOne(array $attributes = [])
+ * @method static Employee[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Employee[]|Proxy[] createSequence(array|callable $sequence)
+ * @method static Employee|Proxy find(object|array|mixed $criteria)
+ * @method static Employee|Proxy findOrCreate(array $attributes)
+ * @method static Employee|Proxy first(string $sortedField = 'id')
+ * @method static Employee|Proxy last(string $sortedField = 'id')
+ * @method static Employee|Proxy random(array $attributes = [])
+ * @method static Employee|Proxy randomOrCreate(array $attributes = [])
+ * @method static Employee[]|Proxy[] all()
+ * @method static Employee[]|Proxy[] findBy(array $attributes)
+ * @method static Employee[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Employee[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static EmployeeRepository|RepositoryProxy repository()
+ * @method Employee|Proxy create(array|callable $attributes = [])
+ */
+final class EmployeeFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'email' => self::faker()->text(),
+            'roles' => [],
+            'firstName' => self::faker()->text(),
+            'lastName' => self::faker()->text(),
+            'password' => self::faker()->text(),
+            'jobTitle' => self::faker()->text(),
+            'jobLocation' => self::faker()->text(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(Employee $employee): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Employee::class;
+    }
+}
