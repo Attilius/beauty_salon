cb67baca55291e694078f1cdf9f94445c355a2f2
Attilius <leo20@freemail.hu>
2022-11-23 21:31:47 +0100

    Modified ProductsController.php

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,5 +16,13 @@ class ProductsController extends AbstractController
     public function index(): Response
     {
         return $this->render('products/index.html.twig', []);
+    }
+
+    #[Route('/products/lipstick', name: 'app_product')]
+    public function showProduct(Product $product): Response
+    {
+        return $this->render('products/product.html.twig', [
+            "product" => $product
+        ]);
     }
 }
