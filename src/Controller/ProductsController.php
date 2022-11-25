03e138b159ca1704a16bccf995263808479ab695
Attilius <leo20@freemail.hu>
2022-11-25 21:21:15 +0100

    Modified ProductsController.php

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductGroupRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +15,27 @@ class ProductsController extends AbstractController
      * @Route("/products", name="app_products")
      * @return Response
      */
-    public function index(): Response
+    public function index(ProductGroupRepository $productGroupRepository): Response
     {
-        return $this->render('products/index.html.twig', []);
+        $product_groups = $productGroupRepository->findAll();
+
+        return $this->render('products/index.html.twig', [
+            "product_groups" => $product_groups
+        ]);
     }
 
-    #[Route('/products/lipstick', name: 'app_product')]
-    public function showProduct(Product $product): Response
+    #[Route('/products/{type}', name: 'app_product')]
+    public function showProduct(ProductRepository $productRepository, string $type): Response
     {
+        $prod = $productRepository->findAll();
+        $selected_products = array();
+        foreach ($prod as $item){
+            if ($item->getType() === $type) {
+                $selected_products[] = $item;
+            }
+        }
         return $this->render('products/product.html.twig', [
-            "product" => $product
+            "product" => $selected_products
         ]);
     }
 }
